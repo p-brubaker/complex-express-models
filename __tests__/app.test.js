93c8baacf0d76d02441b582e330e981be5948d04
Paul Brubaker <pbrubak@gmail.com>
2021-10-04 15:17:19 -0700

    add an animal route passing test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -16,6 +16,19 @@ async function saveSpecies() {
     );
 }
 
+async function saveAnimals() {
+    const testAnimals = [
+        { animalName: 'Vinny', speciesId: '1' },
+        { animalName: 'Larry', speciesId: '2' },
+    ];
+    return Promise.all(
+        testAnimals.map(async (animal) => {
+            const res = await request(app).post('/api/animals').send(animal);
+            return res.body;
+        })
+    );
+}
+
 describe('demo routes', () => {
     beforeEach(() => {
         return setup(pool);
@@ -24,23 +37,32 @@ describe('demo routes', () => {
     it('should add a new species to the species table', async () => {
         const res = await saveSpecies();
         expect(res[0]).toEqual({
-            id: '1',
+            species_id: '1',
             speciesName: 'velociraptor',
         });
     });
 
     it('should get all the species', async () => {
         const res = await saveSpecies();
-        expect(res).toEqual(
+        expect(res).toEqual([
             {
-                id: '1',
+                species_id: '1',
                 speciesName: 'velociraptor',
             },
             {
-                id: '2',
+                species_id: '2',
                 speciesName: 'cow',
-            }
-        );
+            },
+        ]);
+    });
+
+    it('should add a new Animal', async () => {
+        await saveSpecies();
+        const res = await saveAnimals();
+        expect(res).toEqual([
+            { animalName: 'Vinny', animal_id: '1', speciesId: '1' },
+            { animalName: 'Larry', animal_id: '2', speciesId: '2' },
+        ]);
     });
 
     afterAll(() => {
--- sql/setup.sql
+++ sql/setup.sql
@@ -1,6 +1,13 @@
+DROP TABLE IF EXISTS animals CASCADE;
 DROP TABLE IF EXISTS species CASCADE;
 
 CREATE TABLE species (
-    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    species_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
     species_name TEXT
-)
+);
+CREATE TABLE animals (
+    animal_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    animal_name TEXT,
+    species_id BIGINT NOT NULL,
+    FOREIGN KEY (species_id) REFERENCES species (species_id)
+);
