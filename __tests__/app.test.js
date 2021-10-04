0e77f0428dbfb82976381beb60e142e4e33e0821
Paul Brubaker <pbrubak@gmail.com>
2021-10-04 14:18:26 -0700

    route to add new Species passing test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,12 +3,29 @@ import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
 
-describe('demo routes', () => {
-  beforeEach(() => {
-    return setup(pool);
-  });
+async function saveSpecies() {
+    const testSpecies = [{ speciesName: 'velociraptor' }];
+    return Promise.all(
+        testSpecies.map(async (species) => {
+            return request(app).post('/api/species').send(species);
+        })
+    );
+}
 
-  afterAll(() => {
-    pool.end();
-  });
+describe('demo routes', () => {
+    beforeEach(() => {
+        return setup(pool);
+    });
+
+    it('should add a new species to the species table', async () => {
+        const res = await saveSpecies();
+        expect(res[0].body).toEqual({
+            id: '1',
+            speciesName: 'velociraptor',
+        });
+    });
+
+    afterAll(() => {
+        pool.end();
+    });
 });
